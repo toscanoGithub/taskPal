bbdf46d366b0944ab5128a30e888a9c71cd5689c
toscanoGithub <pasarela70@hotmail.com>
2025-04-02 23:38:32 -0400

    auth screen

---
+++ app/(screens)/parent-screen.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ParentScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>ParentScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default ParentScreen
+
+const styles = StyleSheet.create({})
